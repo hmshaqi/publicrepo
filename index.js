715161d97cb31d332ecc15e29d37613af8fdc7ff
gabrielmcg <gabriel.mcgoldrick@hpe.com>
2019-03-04 13:43:03 +0000

    add comput-intensive endpoint

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ app.get("/", (req, res) => {
   res.send(`Hello ${target}!`);
 });
 
+
+app.get("/cpu-intensive", (req, res) => {
+ let x = 0.0001;
+  for (i = 0; i <= 1000000; i++) {
+    x += Math.sqrt(x);
+  }
+  res.send("OK!");
+});
+
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log("Hello world listening on port", port);
